faa211a04255519a1ff5c73317129f0542913a89
Veronika Scheuerová <veronikascheuerova@gmail.com>
2021-04-30 18:51:47 +0200

    disabled double click zooming

--- src/protvista/limited-track.ts
+++ src/protvista/limited-track.ts
@@ -1,6 +1,7 @@
 import ProtvistaTrack from "protvista-track";
 import LimitedNonOverlappingLayout from "./limited-non-overlapping-layout";
 import DefaultLayout from "protvista-track/src/DefaultLayout";
+import d3 = require('d3');
 
 
 export default class LimitedTrack extends ProtvistaTrack {
@@ -11,6 +12,12 @@ export default class LimitedTrack extends ProtvistaTrack {
       });
     return new DefaultLayout({
       layoutHeight: this._height,
+    });
+  }
+  public connectedCallback() {
+    super.connectedCallback();
+    this.zoom.filter(function () {
+      return !d3.event.button && d3.event.type != "dblclick";
     });
   }
 }
